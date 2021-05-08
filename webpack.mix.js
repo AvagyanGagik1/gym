020ac617cc3021678b032df26b42d3978ad72cd0
Doc <arman.saftalyan@gmail.com>
2021-05-08 19:56:05 +0400

    Arman dalbaeb

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,4 +17,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/front/front.scss', 'public/css/front')
     .sass('resources/sass/front/media.scss', 'public/css/front')
-    .copy('resources/fonts/*', 'public/fonts')
+    .sass('resources/sass/owl.scss', 'public/css/package')
+    .copy('resources/fonts/*', 'public/fonts');
