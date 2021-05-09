7babf36df14342dad637514a9b9a2359f82d23d6
Doc <arman.saftalyan@gmail.com>
2021-05-09 13:58:19 +0400

    Arman dalbaeb

---
+++ resources/views/front/user/functional.blade.php
@@ -0,0 +1,285 @@
+@extends('layouts.front.index')
+@section('content')
+    <section class="content functional-content">
+        <div class="d-flex flex-column  ">
+            <div class="col-10 d-flex justify-content-center flex-wrap m-auto">
+                <div class="col-12 p-0 d-flex align-items-center justify-content-between subscribe-header">
+                    <h2>Функциональные тренировки</h2>
+                    <img src="images/cardioHome.png" alt="">
+                </div>
+                <div class="col-12 d-flex justify-content-between align-items-center content-user">
+                    <div class="d-flex align-items-center content-user">
+                        <div  class="content-user-image">
+                            <img src="images/roundUser.png" alt="">
+                        </div>
+                        <div class="d-flex flex-column ">
+                            <p>Тренер</p>
+                            <h1>Лера Алёшкина</h1>
+                        </div>
+                    </div>
+
+                    <div class="d-flex align-items-center">
+                        <div class="d-flex justify-content-center align-items-center content-user">
+                            <div  class="content-user-image-small">
+                                <img src="images/graphRed.png" alt="">
+                            </div>
+                            <div class="d-flex flex-column ">
+                                <p>Интенсивность</p>
+                                <h1>Низкая</h1>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center align-items-center content-user">
+                            <div  class="content-user-image-small">
+                                <img src="images/UserRed.png" alt="">
+                            </div>
+                            <div class="d-flex flex-column ">
+                                <p>Подписано</p>
+                                <h1>2 123</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 d-flex justify-content-between align-items-center content-user-hide">
+                    <button>
+                        Скрыть описание
+                        <img src="images/vectorTop.png" alt="">
+                    </button>
+                </div>
+
+                <div class="col-12 d-flex justify-content-between
+        content-user-preview">
+                    <div class="col-5 pl-0">
+                        <img src="images/previewPlaceholder.png" alt="">
+                    </div>
+                    <div class="col-7 pr-0">
+                        <p>
+                            Хотите похудеть? Привести свое тело в форму? Сжечь лишние калории?
+                            Поставьте перед собой цель и посмотрите, как Gym project может помочь вам преобразовать вашу жизнь. Получите онлайн-доступ ко всем программам тренировок для любого телосложения и любого уровня подготовки.
+                            Хотите похудеть? Привести свое тело в форму? Сжечь лишние калории? Поставьте перед собой цель и посмотрите, как Gym project может помочь вам преобразовать вашу жизнь. Получите онлайн-доступ ко всем программам тренировок для любого телосложения и любого уровня подготовки.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 d-flex justify-content-between
+            align-items-start
+            content-user-video
+             p-0">
+                    <div class="col-7 pl-0 ">
+                        <div class="col-12 p-0 d-flex justify-content-center youtube-header">
+                            <div class="col-8 p-0">
+                                <h3>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h3>
+                            </div>
+                            <div class="col-4  p-0 d-flex justify-content-end">
+                                <div class="youtube-image-ccal">
+                                    <div>
+                                        <img src="images/fire3.png" alt="">
+                                    </div>
+                                    <p>2 145 ccal</p>
+                                </div>
+                                <div class="youtube-image-ccal">
+                                    <div>
+                                        <img src="images/like2.png" alt="">
+                                    </div>
+                                    <p>2 145</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-12 p-0 d-flex align-items-center justify-content-center">
+                            <div class="col-6">
+                                <h1>Разминка:</h1>
+                                <ul>
+                                    <li>— Суставная разминка</li>
+                                    <li> — 5 мин. двойные прыжки на скакалке</li>
+                                </ul>
+                                <h1>Задание 1:</h1>
+                                <ul>
+                                    <li>— Табата 4 мин.</li>
+                                    <li>— 20 сек. супермен</li>
+                                    <li>— 10 сек. отдых</li>
+                                </ul>
+                                <h1>Задание 2:</h1>
+                                <ul>
+                                    <li>— 20 мин. (Максимальное кол-во раундов) </li>
+                                    <li>— 20 рывок двух гантелей с пола 3 кг</li>
+                                    <li> — 20 разведений гантелей в стороны 3кг</li>
+                                    <li> — 20 отжиманий</li>
+                                    <li> — 5-7 мин.</li>
+                                    <li> — 25 подтягивания на перекладине</li>
+                                    <li> — Заминка 7 мин. (разтяжка)</li>
+
+                                </ul>
+                            </div>
+                            <div class="col-6">
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+                                <div class="funtional-training">
+                                    <h2>Упражнения с резинкой для верхней части тела</h2>
+                                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="25" cy="25" r="22.5" fill="#E21A3D"/>
+                                        <path d="M20.834 17.7085V32.2918L32.2923 25.0002L20.834 17.7085Z" fill="white"/>
+                                        <circle cx="25" cy="25" r="24.5" stroke="#E21A3D"/>
+                                    </svg>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="col-12 d-flex justify-content-center youtube-save">
+                            <div class="col-3 p-0 d-flex justify-content-center align-items-center">
+                                <button >ГОТОВО</button>
+                            </div>
+                            <div class="col-9  p-0 d-flex justify-content-end">
+                                <h2>
+                                    После выполнения тренировки нажмите кнопку “Готово” что бы сохранить свой прогресс
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex p-0 flex-wrap youtube-comment-write">
+                            <h2>Оставить отзыв</h2>
+                            <textarea name="" placeholder="Комментарий"></textarea>
+                            <button>
+                                отправить отзыв
+                            </button>
+                        </div>
+                        <div class="col-12 d-flex p-0 flex-wrap youtube-comment">
+                            <h1>Отзывы <span>(13)</span></h1>
+
+                            <div class="col-12 p-0">
+                                <div class="col-12 p-0 d-flex align-items-center">
+                                    <div>
+                                        <img src="images/previewPlaceholder.png" alt="">
+                                    </div>
+                                    <h3>Юлия Стриж</h3>
+                                </div>
+                                <p>Хотите похудеть? Привести свое тело в форму? Сжечь лишние калории? Поставьте перед собой цель и посмотрите, как Gym project может помочь вам преобразовать вашу жизнь. Получите онлайн-доступ ко всем программам тренировок для любого телосложения и любого уровня подготовки.</p>
+                                <a href="">Ответить</a>
+                            </div>
+                            <div class="col-12 p-0 answer">
+                                <div class="col-12 p-0 d-flex align-items-center">
+                                    <div>
+                                        <img src="images/previewPlaceholder.png" alt="">
+                                    </div>
+                                    <h3>Юлия Стриж</h3>
+                                </div>
+                                <p>Хотите похудеть? Привести свое тело в форму? Сжечь лишние калории? Поставьте перед собой цель и посмотрите, как Gym project может помочь вам преобразовать вашу жизнь. Получите онлайн-доступ ко всем программам тренировок для любого телосложения и любого уровня подготовки.</p>
+                                <a href="">Ответить</a>
+                            </div>
+
+                            <button >
+                                отправить отзыв
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-5 ">
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-12 d-flex">
+                            <div class="col-6 pl-0">
+                                <img src="images/previewPlaceholder.png" class="w-100" alt="">
+                            </div>
+                            <div class="col-6 pr-0">
+                                <h2>
+                                    Лекция 1. Понятие рационального питания и здоровья
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="youtube-programm col-12">
+                            <button>
+                                завершить программу
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+@endsection
