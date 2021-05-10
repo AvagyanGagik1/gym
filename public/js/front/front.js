3f2faa095845ebf17a683f78f8c14d1346ea36ca
Doc <arman.saftalyan@gmail.com>
2021-05-10 18:14:22 +0400

    Arman

---
+++ public/js/front/front.js
@@ -0,0 +1,90 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************************!*\
+  !*** ./resources/js/front/front.js ***!
+  \*************************************/
+$(document).ready(function () {
+  $('#owl-carousel').owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    margin: 20,
+
+    /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+  $('#owl-carousel-program').owlCarousel({
+    responsiveClass: true,
+    margin: 20,
+    lazyLoad: true,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1.2,
+        nav: false
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },
+      1100: {
+        items: 4
+      }
+    }
+  });
+  $('#owl-carousel-trainer').owlCarousel({
+    responsiveClass: true,
+    margin: 20,
+    lazyLoad: true,
+    smartSpeed: 800,
+    nav: true,
+    loop: false,
+    responsive: {
+      0: {
+        items: 1.2
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },
+      1100: {
+        items: 4
+      }
+    }
+  });
+  $('#olw-carousel-news').owlCarousel({
+    responsiveClass: true,
+    margin: 20,
+    lazyLoad: true,
+    smartSpeed: 800,
+    nav: true,
+    loop: false,
+    responsive: {
+      0: {
+        items: 1.2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
+/******/ })()
+;
