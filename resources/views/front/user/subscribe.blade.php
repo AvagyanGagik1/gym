d3ed3282aca7a33ab0252dbeb15d9f358361cd2d
Doc <arman.saftalyan@gmail.com>
2021-05-10 21:50:15 +0400

    Arman

--- resources/views/front/user/subscribe.blade.php
+++ resources/views/front/user/subscribe.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="content subscribe-content">
         <div class="d-flex flex-column  ">
-            <div class="col-xl-10 col-12 d-flex justify-content-center flex-wrap m-auto">
+            <div class="col-xl-10 col-12 d-flg-l col-12ex justify-content-center flex-wrap m-auto">
                 <div class="col-12 p-0 d-flex align-items-center subscribe-header">
                     <h2>Мои подписки</h2>
                 </div>
@@ -24,8 +24,8 @@
                             Выбрать новый план подписок
                         </h2>
                     </div>
-                    <div class="subscribe-new-plan-price d-flex col-12 p-0 justify-content-between flex-wrap">
-                        <div class="col-6">
+                    <div class="subscribe-new-plan-price d-flex col-12 p-0 justify-content-center flex-wrap">
+                        <div class="col-lg-6 col-12">
                             <h4>
                                 4 Недельная Трансформация
                             </h4>
@@ -34,7 +34,7 @@
                                 ПРОДЛИТЬ
                             </button>
                         </div>
-                        <div class="col-6">
+                        <div class="col-lg-6 col-12">
                             <h4>
                                 4 Недельная Трансформация
                             </h4>
@@ -43,7 +43,7 @@
                                 ПРОДЛИТЬ
                             </button>
                         </div>
-                        <div class="col-6">
+                        <div class="col-lg-6 col-12">
                             <h4>
                                 4 Недельная Трансформация
                             </h4>
@@ -52,7 +52,7 @@
                                 ПРОДЛИТЬ
                             </button>
                         </div>
-                        <div class="col-6">
+                        <div class="col-lg-6 col-12">
                             <h4>
                                 4 Недельная Трансформация
                             </h4>
