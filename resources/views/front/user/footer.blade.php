448d889fe8c7518864e79c56cbcd4f1edd8df7c8
Doc <arman.saftalyan@gmail.com>
2021-05-10 18:09:23 +0400

    Arman

--- resources/views/front/user/footer.blade.php
+++ resources/views/front/user/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="userFooter">
     <div class="col-12">
-        <div class="col-10 d-flex justify-content-between m-auto">
-            <div>
+        <div class="col-10 d-flex justify-content-between m-auto user-content">
+            <div class="copyright">
                 <p class="m-0">Copyright © 2018-2021 Online Gym project. All Rights Reserved.
                 </p>
             </div>
-            <div class="d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center promodex">
                 <p class="m-0 mr-2">Разработано:</p><img src="images/PROMODEX.png" alt="">
             </div>
         </div>
